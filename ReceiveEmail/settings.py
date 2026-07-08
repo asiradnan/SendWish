9fa3c9f57af5c8891b05ed291ce3cc0959647273
asiradnan <asiradnan23@gmail.com>
2026-07-08 19:31:07 +0600

    add secure proxy ssl header

--- ReceiveEmail/settings.py
+++ ReceiveEmail/settings.py
@@ -6,7 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 SECRET_KEY = os.environ.get("secret_key")
 DEBUG = False
+
 ALLOWED_HOSTS = ["127.0.0.1",".asiradnan.com"]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
